d829a1691c3781855c8add58be6b85fc016692a8
Leo P <junk@slact.net>
2017-10-08 20:35:53 -0400

    yeap

--- ext/teensy_rawhid/extconf.rb
+++ ext/teensy_rawhid/extconf.rb
@@ -8,12 +8,15 @@ $srcs = ["#{extension_name}.c"]
 case Gem::Platform.local.os
 when "linux"
   have_library "usb", nil, ["usb.h"]
-  $defs.push("-DOS_LINUX")
+  $defs.push "-DOS_LINUX"
   $srcs << "hid_LINUX.c"
+#when "darwin"
+#  raise "can't build for 
+#  have_library "hid", nil, ["hid.h"]
+#  $defs.push "-DOS_MACOSX"
 else
-  exit 1
+  raise "Don't know ho to build rawhid on #{Gem::Platform.local.os} yet."
 end
-
 
 create_header
 create_makefile "#{extension_name}"
